2fd01eee75de3d83bcb6444d28996ba306e1c986
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2023-06-25 19:12:24 +0300

    Crystalline Conflict section added

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-// Function to update the events and time of change for the next 30 days
+// Function to update the events and time of change for the next 14 days
 function updateEvents() {
     var eventList = document.getElementById("event-list");
     eventList.innerHTML = ""; // Clear the existing event list
+
+    var eventListCC = document.getElementById("event-list-cc");
+    eventListCC.innerHTML = ""; // Clear the existing CC event list
 
     var currentDate = new Date();
     var currentHour = currentDate.getHours();
 
     // Get today's event
     var events = ["Seize", "Shatter", "Onsal"];
+    var eventsCC = ["CC"];
     var eventChangeHour = 18; // Events change at 6 PM
     var todayEvent = events[currentDate.getDate() % events.length];
+    var todayEventCC = eventsCC[currentDate.getDate() % eventsCC.length];
 
     var listItem = document.createElement("li");
     listItem.textContent = "Today's Event: " + todayEvent;
     listItem.classList.add("event-" + todayEvent.toLowerCase());
     eventList.appendChild(listItem);
 
-    // Get the next 30 days
-    for (var i = 1; i <= 30; i++) {
+    var listItemCC = document.createElement("li");
+    listItemCC.textContent = "Today's CC Event: " + todayEventCC;
+    listItemCC.classList.add("event-cc");
+    eventListCC.appendChild(listItemCC);
+
+    // Get the next 14 days
+    for (var i = 1; i <= 14; i++) {
         var nextDate = new Date(currentDate);
         nextDate.setDate(currentDate.getDate() + i);
 
@@ -33,11 +43,18 @@ function updateEvents() {
         listItem.textContent = eventText + " (Change at " + eventTime + " on " + eventDate + ")";
         listItem.classList.add("event-" + eventText.toLowerCase());
         eventList.appendChild(listItem);
+
+        var listItemCC = document.createElement("li");
+        var eventTextCC = eventsCC[(currentDate.getDate() + i) % eventsCC.length];
+        listItemCC.textContent = eventTextCC + " (Change at " + eventTime + " on " + eventDate + ")";
+        listItemCC.classList.add("event-cc");
+        eventListCC.appendChild(listItemCC);
     }
 
     // Schedule the next update
     setTimeout(updateEvents, 60000); // Check every minute
 }
+
 
 // Helper function to format the date as "Day, Month Date"
 function formatDate(date) {
